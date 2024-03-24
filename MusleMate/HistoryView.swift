66015155ea41f73e12377eb219fa626fcd7f78ed
piyo.temix <piyo.temix@gmail.com>
2024-03-24 16:49:54 +0900

    一旦コミット

--- MusleMate/HistoryView.swift
+++ MusleMate/HistoryView.swift
@@ -9,51 +9,69 @@ import SwiftUI
 // WorkoutItem構造体を定義し、Identifiableに準拠させる
 struct WorkoutItem: Identifiable {
     let id = UUID()
+    let date: String  // 日付
     let time: String
     let menu: String
-    let weight: Int
+    let weight: Double
     let unit: String
+    let reps: Int
 }
 
 struct HistoryView: View {
     // WorkoutItem型の配列を格納するプロパティ
-    @State private var workoutItems: [WorkoutItem] = []
+    @State private var workoutItems: [String: [WorkoutItem]] = [:] // 日付ごとのワークアウトアイテムを格納する辞書
     @State private var selectedDate = Date() // 選択された日付
-    @State private var memoText = "" // メモのテキスト
 
     // ボディ部分
     var body: some View {
         VStack {
-            // カレンダー
-            DatePicker("Selected Date", selection: $selectedDate, displayedComponents: .date)
-                .datePickerStyle(GraphicalDatePickerStyle())
-                .padding()
-            // ヘッダー
-            HStack {
-                Text("Time").frame(maxWidth: .infinity)
-                Text("Menu").frame(maxWidth: .infinity)
-                Text("Weight").frame(maxWidth: .infinity)
-            }
-            .padding(.horizontal)
-
             // データを表示するForEach
             ScrollView {
                 VStack(alignment: .leading) {
-                    ForEach(workoutItems) { item in
-                        HStack {
-                            Text(item.time).frame(maxWidth: .infinity)
-                            Text(item.menu).frame(maxWidth: .infinity)
-                            Text("\(item.weight) \(item.unit)").frame(maxWidth: .infinity)
+                    ForEach(workoutItems.keys.sorted(), id: \.self) { key in
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text(key)
+                                .font(.headline)
+                                .padding(.leading, 10)
+                                .padding(.top, 10)
+                            ForEach(workoutItems[key]!, id: \.id) { item in
+                                VStack(alignment: .leading, spacing: 5) {
+                                    HStack {
+                                        Image(systemName: "figure.cross.training")
+                                            .font(.system(size: 30))
+                                            .padding(.trailing, 5)
+                                        VStack(alignment: .leading, spacing: 5) {
+                                            HStack {
+                                                Text(item.menu)
+                                                    .fontWeight(.bold)
+                                                    .lineLimit(1)
+                                                Spacer()
+                                                Text("\(item.date) \(item.time)")  // 日付と時間を結合して表示
+                                                    .foregroundColor(.gray)
+                                            }
+                                            .padding(.vertical, 5)
+                                            HStack {
+                                                Text(String(format: "%.2f", item.weight) + "\(item.unit) lb")
+                                                    .foregroundColor(.blue)
+                                                Text("\(item.reps) reps")
+                                                    .foregroundColor(.blue)
+                                            }
+                                            .padding(.vertical, 5)
+                                        }
+                                    }
+                                    .padding(.horizontal, 10)
+                                    .padding(.vertical, 5)
+                                }
+                            }
                         }
-                        .padding(.horizontal)
                     }
                 }
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray.opacity(0.5)))
+                .padding(.horizontal, 10)
+                .padding(.vertical, 5)
+
             }
-            
-            // メモ入力欄
-            TextEditor(text: $memoText)
-                .frame(height: 100)
-                .padding()
         }
         .navigationTitle("Workout History")
         .onAppear {
@@ -69,29 +87,39 @@ struct HistoryView: View {
         // workout_から始まるキーをフィルタリングしてソート
         let keys = userDefaults.dictionaryRepresentation().keys.filter { $0.starts(with: "workout_") }.sorted()
 
-        var items: [WorkoutItem] = []
+        var itemsDictionary: [String: [WorkoutItem]] = [:]
 
         for key in keys {
             // キーが"workout_"で始まり、かつ対応するデータが存在する場合にのみ処理を行う
             if let data = userDefaults.dictionary(forKey: key) {
                 // 各値を取得し、nilの場合はデフォルト値を設定する
                 let menu = data["menu"] as? String ?? ""
-                let weight = data["weight"] as? Int ?? 0
+                let weight = data["weight"] as? Double ?? 0
                 let unit = data["unit"] as? String ?? ""
+                let reps = data["reps"] as? Int ?? 0
                 
-                // 時刻の取得
+                // 日付の取得
                 let dateFormatter = DateFormatter()
                 dateFormatter.dateFormat = "yyyyMMddHHmmss"
                 if let date = dateFormatter.date(from: String(key.suffix(14))) {
+                    dateFormatter.dateFormat = "yyyy/MM/dd"
+                    let dateString = dateFormatter.string(from: date)
+                    
                     dateFormatter.dateFormat = "HH:mm:ss"
                     let time = dateFormatter.string(from: date)
-                    // ワークアウトアイテムを作成してリストに追加
-                    items.append(WorkoutItem(time: time, menu: menu, weight: weight, unit: unit))
+                    
+                    // ワークアウトアイテムを作成して辞書に追加
+                    let workoutItem = WorkoutItem(date: dateString, time: time, menu: menu, weight: weight, unit: unit, reps: reps)
+                    if itemsDictionary[dateString] != nil {
+                        itemsDictionary[dateString]!.append(workoutItem)
+                    } else {
+                        itemsDictionary[dateString] = [workoutItem]
+                    }
                 }
             }
         }
 
-        workoutItems = items
+        workoutItems = itemsDictionary
     }
 }
 
