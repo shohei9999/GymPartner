913899ad36b1a7c7a55634bc45c6479313943c3e
piyo.temix <piyo.temix@gmail.com>
2024-04-09 22:00:39 +0900

    applewatch側の受信処理をtransferUserInfoにあった形に修正

--- MusleMate Watch App/WatchSessionDelegate.swift
+++ MusleMate Watch App/WatchSessionDelegate.swift
@@ -63,6 +63,18 @@ class WatchSessionDelegate: NSObject, ObservableObject, WCSessionDelegate {
         }
     }
     
+    func session(_ session: WCSession, didReceiveUserInfo userInfo: [String : Any] = [:]) {
+        if let receivedData = userInfo["data"] as? [String] {
+            // 受信したデータを更新
+            DispatchQueue.main.async {
+                self.receivedData = receivedData
+                print("received data from iPhone")
+                // UserDefaultsにデータを保存
+                UserDefaults.standard.set(receivedData, forKey: self.userDefaultsKey)
+            }
+        }
+    }
+    
     // iPhoneにメッセージを送信するメソッド
     func sendMessageToiPhone() {
         // ワークアウトのデータを格納する配列
@@ -74,20 +86,33 @@ class WatchSessionDelegate: NSObject, ObservableObject, WCSessionDelegate {
 
         // キーに対応するデータを取得して配列に保存
         for key in keys {
-            if let data = userDefaults.dictionary(forKey: key), let sendStatus = data["sendStatus"] as? Bool, !sendStatus {
-                workoutDataArray.append(["key": key, "data": data])
-            }
+          if let data = userDefaults.dictionary(forKey: key), let sendStatus = data["sendStatus"] as? Bool, !sendStatus {
+              workoutDataArray.append(["key": key, "data": data])
+          }
         }
-        
+
         // workoutDataArrayが空の場合は送信処理を行わない
         guard !workoutDataArray.isEmpty else {
-            print("workoutDataArray is empty. Skipping sending message to iPhone.")
-            return
+          print("workoutDataArray is empty. Skipping sending message to iPhone.")
+          return
         }
-        
+
         print("iPhoneにデータ送信： \(workoutDataArray)")
 
         // iPhoneにメッセージを送信
-        session.transferUserInfo(["workoutDataArray": workoutDataArray])
+        session.sendMessage(["workoutDataArray": workoutDataArray], replyHandler: { replyMessage in
+          if let keysArray = replyMessage["keys"] as? [String] {
+              for key in keysArray {
+                  print("Key: \(key)")
+                  if var data = UserDefaults.standard.dictionary(forKey: key) {
+                      data["sendStatus"] = true
+                      UserDefaults.standard.set(data, forKey: key)
+                  }
+              }
+          }
+        }, errorHandler: { error in
+          // メッセージの送信が失敗した場合の処理
+          print("Error sending message: \(error.localizedDescription)")
+        })
     }
 }
