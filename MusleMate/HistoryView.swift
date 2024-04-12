41a6d42e2a69cfa080929cd8b4c0c5dede047a17
piyo.temix <piyo.temix@gmail.com>
2024-04-12 14:47:55 +0900

    iphoneの履歴も削除できるよう修正

--- MusleMate/HistoryView.swift
+++ MusleMate/HistoryView.swift
@@ -6,76 +6,74 @@
 //
 import SwiftUI
 
-// WorkoutItem構造体を定義し、Identifiableに準拠させる
-struct WorkoutItem: Identifiable {
-    let id = UUID()
-    let date: String  // 日付
-    let time: String
-    let menu: String
-    let weight: Double
-    let unit: String
-    let reps: Int
-}
-
 struct HistoryView: View {
+    // WorkoutItem構造体を定義し、Identifiableに準拠させる
+    struct WorkoutItem: Identifiable {
+        let id = UUID()
+        let date: String  // 日付
+        let time: String
+        let menu: String
+        let weight: Double
+        let unit: String
+        let reps: Int
+    }
+    
     // WorkoutItem型の配列を格納するプロパティ
-    @State private var workoutItems: [String: [WorkoutItem]] = [:] // 日付ごとのワークアウトアイテムを格納する辞書
+    @State private var workoutItems: [WorkoutItem] = [] // ワークアウトアイテムを格納する配列
     var selectedDate: Date // 選択された日付を受け取る
     
     // ボディ部分
     var body: some View {
         VStack {
-            // データを表示するForEach
-            ScrollView {
-                VStack(alignment: .leading) {
-                    if workoutItems.isEmpty {
-                        Text("Selected date does not have any training records.")
-                            .font(.body) // フォントをbodyに設定
-                            .padding()
-                    } else {
-                        ForEach(workoutItems.keys.sorted(), id: \.self) { key in
-                            VStack(alignment: .leading, spacing: 10) {
-                                Text(key)
-                                    .font(.headline)
-                                    .padding(.leading, 10)
-                                    .padding(.top, 10)
-                                ForEach(workoutItems[key]!, id: \.id) { item in
-                                    VStack(alignment: .leading, spacing: 5) {
-                                        HStack {
-                                            Image(systemName: "figure.cross.training")
-                                                .font(.system(size: 30))
-                                                .padding(.trailing, 5)
-                                            VStack(alignment: .leading, spacing: 5) {
-                                                HStack {
-                                                    Text(item.menu)
-                                                        .fontWeight(.bold)
-                                                        .lineLimit(1)
-                                                    Spacer()
-                                                    Text("\(item.date) \(item.time)")  // 日付と時間を結合して表示
-                                                        .foregroundColor(.gray)
-                                                }
-                                                .padding(.vertical, 5)
-                                                HStack {
-                                                    Text(String(format: "%.2f", item.weight) + "\(item.unit)")
-                                                        .foregroundColor(.blue)
-                                                    Text("\(item.reps) reps")
-                                                        .foregroundColor(.blue)
-                                                }
-                                                .padding(.vertical, 5)
-                                            }
-                                        }
-                                        .padding(.horizontal, 10)
-                                        .padding(.vertical, 5)
-                                    }
+            if workoutItems.isEmpty {
+                VStack {
+                    Text("Selected date does not have any training records.")
+                        .font(.body) // フォントをbodyに設定
+                        .padding()
+                    Spacer()
+                }
+            } else {
+                // データがある場合はリストを表示
+                List {
+                    ForEach(workoutItems) { item in
+                        HStack {
+                            Image(systemName: "figure.cross.training")
+                                .font(.system(size: 30))
+                                .padding(.trailing, 5)
+                            VStack(alignment: .leading, spacing: 5) {
+                                HStack {
+                                    Text(item.menu)
+                                        .fontWeight(.bold)
+                                        .lineLimit(1)
+                                    Spacer()
+                                    Text("\(item.date) \(item.time)")  // 日付と時間を結合して表示
+                                        .foregroundColor(.gray)
                                 }
+                                .padding(.vertical, 5)
+                                HStack {
+                                    Text(String(format: "%.2f", item.weight) + "\(item.unit)")
+                                        .foregroundColor(.blue)
+                                    Text("\(item.reps) reps")
+                                        .foregroundColor(.blue)
+                                }
+                                .padding(.vertical, 5)
                             }
-                            .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
-                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray.opacity(0.5)))
-                            .padding(.horizontal, 10)
-                            .padding(.vertical, 5)
+                        }
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 5)
+                        // 右にスワイプして削除
+                        .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                            Button(role: .destructive) {
+                                // 削除処理を実行
+                                deleteWorkoutItem(item)
+                            } label: {
+                                // 削除ボタンの表示
+                                Label("Delete", systemImage: "trash")
+                            }
                         }
                     }
                 }
+                .listStyle(PlainListStyle()) // リストスタイルをプレーンに設定
             }
         }
         .onAppear {
@@ -94,7 +92,7 @@ struct HistoryView: View {
         // workout_から始まるキーをフィルタリングしてソート
         let keys = userDefaults.dictionaryRepresentation().keys.filter { $0.starts(with: "workout_") && $0.contains(formattedDate) }.sorted()
         print("keys: \(keys)")
-        var itemsDictionary: [String: [WorkoutItem]] = [:]
+        var items: [WorkoutItem] = []
         
         for key in keys {
             // キーが"workout_"で始まり、かつ対応するデータが存在する場合にのみ処理を行う
@@ -115,21 +113,33 @@ struct HistoryView: View {
                     dateFormatter.dateFormat = "HH:mm:ss"
                     let time = dateFormatter.string(from: date)
                     
-                    // ワークアウトアイテムを作成して辞書に追加
+                    // ワークアウトアイテムを作成して配列に追加
                     let workoutItem = WorkoutItem(date: dateString, time: time, menu: menu, weight: weight, unit: unit, reps: reps)
-                    if itemsDictionary[dateString] != nil {
-                        itemsDictionary[dateString]!.append(workoutItem)
-                    } else {
-                        itemsDictionary[dateString] = [workoutItem]
-                    }
+                    items.append(workoutItem)
                 }
             }
         }
         
-        workoutItems = itemsDictionary
+        workoutItems = items
+    }
+    
+    // ワークアウトアイテムを削除するメソッド
+    private func deleteWorkoutItem(_ item: WorkoutItem) {
+        // 削除対象のアイテムのインデックスを取得
+        if let index = workoutItems.firstIndex(where: { $0.id == item.id }) {
+            // 配列から削除
+            workoutItems.remove(at: index)
+            
+            // UserDefaultsからも削除
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyyMMdd"
+            let formattedDate = dateFormatter.string(from: selectedDate)
+            let userDefaults = UserDefaults.standard
+            let key = "workout_\(formattedDate)\(item.time.replacingOccurrences(of: ":", with: ""))"
+            userDefaults.removeObject(forKey: key)
+        }
     }
 }
-
 
 #Preview {
     HistoryView(selectedDate: Date())
